5abc9d54bc8cfaee0cedc8aaec8b261827180aed
orangemug <info@orangemug.co.uk>
2016-03-15 12:44:19 +0000

    Added in mocha for test logging

--- test/index.js
+++ test/index.js
@@ -1,6 +1,12 @@
 var assert       = require("assert");
 var readmeTester = require("readme-tester");
 
-readmeTester(__dirname+"/..", function(err, assertions) {
-  assert(!err);
-});
+
+describe("check-dep", function() {
+  it("README", function(done) {
+    readmeTester(__dirname+"/..", function(err, assertions) {
+      assert(!err);
+      done();
+    });
+  })
+})
